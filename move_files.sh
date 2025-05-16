9ef6b5fe60ba209815dd4e85717c145630c0bd80
Han Yan <yanhan0121@gmail.com>
2025-05-16 16:27:51 +0800

    5/16/25每日一题

--- move_files.sh
+++
@@ -1,5 +0,0 @@
-# move files in a directory based on their extensions and suppress errors
-# Usage: ./move_files.sh
-mv *.py python/ 2>/dev/null
-mv *.cpp cpp/ 2>/dev/null
-mv *.java java/ 2>/dev/null
