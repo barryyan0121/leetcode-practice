7d1359f45bc98f7fa208ae50d944c1767423d1c9
Han Yan <yanhan0121@gmail.com>
2025-05-19 17:01:42 +0800

    接雨水三种做法

---
+++ python/738.单调递增的数字.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=738 lang=python3
+# @lcpr version=30201
+#
+# [738] 单调递增的数字
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+class Solution:
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+#
+# @lcpr case=start
+# 10\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 1234\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 332\n
+# @lcpr case=end
+
+#
+
