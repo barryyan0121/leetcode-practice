00da4725fa6956adc01aee7e0a8339d1cc04d4f2
Han Yan <yanhan0121@gmail.com>
2025-05-27 14:39:47 +0800

    完成5/27/25每日一题

---
+++ python/218.天际线问题.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=218 lang=python3
+# @lcpr version=30201
+#
+# [218] 天际线问题
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+#
+# @lcpr case=start
+# [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[0,2,3],[2,5,3]]\n
+# @lcpr case=end
+
+#
+
