0eb2f49e03fb36b8d2426f67f3a3c262328472af
Han Yan <yanhan0121@gmail.com>
2025-05-28 16:38:41 +0800

    完成每日一题20250528以及两道经典dfs

---
+++ python/3372.连接两棵树后最大目标节点数目 I.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode.cn id=3372 lang=python3
+# @lcpr version=30201
+#
+# [3372] 连接两棵树后最大目标节点数目 I
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+        def dfs(node: int, parent: int, children: List[List[int]], k: int) -> int:
+            if k < 0:
+                return 0
+            res = 1
+            for child in children[node]:
+                if child == parent:
+                    continue
+                res += dfs(child, node, children, k - 1)
+            return res
+
+        def build(edges: List[List[int]], k: int) -> List[int]:
+            n = len(edges) + 1
+            children = [[] for _ in range(n)]
+            for u, v in edges:
+                children[u].append(v)
+                children[v].append(u)
+            res = [0] * n
+            for i in range(n):
+                res[i] = dfs(i, -1, children, k)
+            return res
+
+        n = len(edges1) + 1
+        count1 = build(edges1, k)
+        count2 = build(edges2, k - 1)
+        maxCount2 = max(count2)
+        res = [count1[i] + maxCount2 for i in range(n)]
+        return res
+
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+#
+# @lcpr case=start
+# [[0,1],[0,2],[2,3],[2,4]]\n[[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]]\n2\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[0,1],[0,2],[0,3],[0,4]]\n[[0,1],[1,2],[2,3]]\n1\n
+# @lcpr case=end
+
+#
+
---
+++ python/695.岛屿的最大面积.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=695 lang=python3
+# @lcpr version=30201
+#
+# [695] 岛屿的最大面积
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        res = 0
+        def dfs(i, j) -> int:
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    res = max(res, dfs(i, j))
+        return res
+# @lc code=end
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+#
+# @lcpr case=start
+# \n[[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[0,0,0,0,0,0,0,0]]\n
+# @lcpr case=end
+
+#
