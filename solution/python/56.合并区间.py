20f9364d24285cdac602112ace45b753fcd841d0
Han Yan <yanhan0121@gmail.com>
2025-07-11 10:31:45 +0800

    🐛 fix(.gitignore)：添加.pypirc忽略项并删除.lcpr_data目录下的文件 📝 fix(generate_readme.py)：修复generate_readme函数中获取最后修改时间的参数传递问题 ♻️ remove(solution/.lcpr_data/)：删除不再使用的缓存数据文件（bricks.json和group.json） ✨ feat(solution): 添加LeetCode题目56合并区间与3169无需开会的工作日解决方案

---
+++ solution/python/56.合并区间.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+# @lcpr version=30201
+#
+# [56] 合并区间
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import *
+from common.node import *
+
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        merged = []
+        merged.append(intervals[0])
+        for i in range(1, len(intervals)):
+            current = intervals[i]
+            last_merged = merged[-1]
+            if current[0] <= last_merged[1]:
+                last_merged[1] = max(last_merged[1], current[1])
+            else:
+                merged.append(current)
+        return merged
+# @lc code=end
+        pass
+
+if __name__ == '__main__':
+    solution = Solution()
+    # your test code here
+
+
+
+#
+# @lcpr case=start
+# [[1,3],[2,6],[8,10],[15,18]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[1,4],[4,5]]\n
+# @lcpr case=end
+
+#
+
