1035ae64269e9082e8e5aa768fad1e277ca0ebe4
Lint Action <lint-action@samuelmeuli.com>
2025-08-13 03:15:08 +0000

    Fix code style issues with Black

--- solution/python/1.两数之和.py
+++ solution/python/1.两数之和.py
@@ -17,9 +17,7 @@ from common.node import *
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return [i for i, num in enumerate(nums) if target - num in nums[i + 1 :]] + [
-            i for i, num in enumerate(nums) if target - num in nums[:i]
-        ]
+        return [i for i, num in enumerate(nums) if target - num in nums[i + 1 :]] + [i for i, num in enumerate(nums) if target - num in nums[:i]]
 
         # @lc code=end
         pass
@@ -43,9 +41,7 @@ if __name__ == "__main__":
             print(f"测试用例 {idx + 1} 通过: n = {args}, result = {result}")
         except AssertionError:
             all_passed = False
-            print(
-                f"测试用例 {idx + 1} 失败: n = {args}, 期望 = {expected}, 实际 = {result}"
-            )
+            print(f"测试用例 {idx + 1} 失败: n = {args}, 期望 = {expected}, 实际 = {result}")
 
     file_path = os.path.basename(__file__).split(".")
     file_number = file_path[0]
