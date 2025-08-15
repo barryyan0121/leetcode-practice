8b13eed5b7610bb26333a94b36aecba0b8bdb41a
Han Yan <yanhan0121@gmail.com>
2025-08-15 17:06:05 +0800

    🐛 fix(solution/python/101.对称二叉树.py)：修复导入语句的路径分隔符问题（从斜杠改为点号） 📝 add(solution/python/101.对称二叉树.py)：添加测试用例代码和文件信息输出
    ⬆️ update(lcpr version): 将lcpr版本号从30201更新为30202
    ✅ fix(test cases for 42.接雨水.py): 添加测试用例断言和错误处理，确保测试结果正确显示
    📝 add(solution/python/42.接雨水.py)：添加测试代码框架并输出文件信息
    
    ⬆️ update(lcpr version): 将lcpr版本号从30201更新为30202
    📝 add(test cases and file info for 70.爬楼梯.py): 添加测试用例和文件信息输出，确保测试结果正确显示
    
    ✨ feat(8.字符串转换整数 (atoi)): 实现将字符串转换为整数的功能
    ✅ tests: 添加测试用例以验证函数的正确性

--- solution/python/101.对称二叉树.py
+++ solution/python/101.对称二叉树.py
@@ -1,6 +1,6 @@
 #
 # @lc app=leetcode.cn id=101 lang=python3
-# @lcpr version=30201
+# @lcpr version=30202
 #
 # [101] 对称二叉树
 #
@@ -8,11 +8,10 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from typing import *
-from common.node import *
-
+from common.node import TreeNode
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -35,13 +34,45 @@ class Solution:
             and self.isSymmetricHelper(left.left, right.right)
             and self.isSymmetricHelper(left.right, right.left)
         )
+        # @lc code=end
 
 
-# @lc code=end
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     solution = Solution()
-    # your test code here
+    # 测试用例 (func, args, result)
+    [1, 2, 2, None, 3, None, 3]
+    test_cases = [
+        (
+            solution.isSymmetric,
+            ((TreeNode.create_root([1, 2, 2, 3, 4, 4, 3])),),
+            True,
+        ),
+        (
+            solution.isSymmetric,
+            ((TreeNode.create_root([1, 2, 2, None, 3, None, 4])),),
+            False,
+        ),
+    ]
+
+    all_passed = True
+    for idx, (func, args, expected) in enumerate(test_cases):
+        try:
+            result = func(*args)
+            assert result == expected
+            print(f"测试用例 {idx + 1} 通过: n = {args}, result = {result}")
+        except AssertionError:
+            all_passed = False
+            print(f"测试用例 {idx + 1} 失败: n = {args}, 期望 = {expected}, 实际 = {result}")
+
+    file_path = os.path.basename(__file__).split(".")
+    file_number = file_path[0]
+    file_name = file_path[1]
+    if all_passed:
+        print(f'第 {file_number} 题 "{file_name}" 所有测试用例通过')
+        sys.exit(0)
+    else:
+        print(f'第 {file_number} 题 "{file_name}" 部分测试用例失败')
+        sys.exit(1)
 
 
 #
