6e17978f1a33d73cbdddf99ca2f9c2ecf9e85422
颜涵 <yanhan0121@gmail.com>
2026-07-15 10:43:30 +0800

    Add solution for daily question 3658

---
+++ solution/python/3658.奇数和与偶数和的最大公约数.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=3658 lang=python3
+#
+# [3658] 奇数和与偶数和的最大公约数
+#
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
+
+
+# @lc code=start
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+
+
+# @lc code=end
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    test_cases = [
+        (solution.gcdOfOddEvenSums, (4,), 4),
+        (solution.gcdOfOddEvenSums, (5,), 5),
+    ]
+
+    all_passed = True
+    for idx, (func, args, expected) in enumerate(test_cases):
+        try:
+            result = func(*args)
+            assert result == expected
+            print(f"测试用例 {idx + 1} 通过: n = {args}, result = {result}")
+        except AssertionError:
+            all_passed = False
+            print(
+                f"测试用例 {idx + 1} 失败: n = {args}, 期望 = {expected}, 实际 = {result}"
+            )
+
+    file_path = os.path.basename(__file__).split(".")
+    file_number = file_path[0]
+    file_name = file_path[1]
+    if all_passed:
+        print(f'第 {file_number} 题 "{file_name}" 所有测试用例通过')
+        sys.exit(0)
+    else:
+        print(f'第 {file_number} 题 "{file_name}" 部分测试用例失败')
+        sys.exit(1)
