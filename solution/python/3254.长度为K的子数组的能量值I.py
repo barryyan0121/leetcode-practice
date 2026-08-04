c2bc5e24c4a9876236ce2f2e1bbb2b0354cb9880
颜涵 <yanhan0121@gmail.com>
2026-08-04 10:05:14 +0800

    feat: add solution for problem 3254

---
+++ solution/python/3254.长度为K的子数组的能量值I.py
@@ -0,0 +1,22 @@
+class Solution:
+    def resultsArray(self, nums: list[int], k: int) -> list[int]:
+        increasing = 1
+        answer = []
+        for index in range(len(nums)):
+            if index and nums[index] == nums[index - 1] + 1:
+                increasing += 1
+            else:
+                increasing = 1
+            if index >= k - 1:
+                answer.append(nums[index] if increasing >= k else -1)
+        return answer
+
+
+if __name__ == "__main__":
+    test_cases = [
+        (([1, 2, 3, 4, 3, 2, 5], 3), [3, 4, -1, -1, -1]),
+        (([2, 2, 2, 2, 2], 4), [-1, -1]),
+        (([3, 2, 3, 2, 3, 2], 2), [-1, 3, -1, 3, -1]),
+    ]
+    for _, ((nums, k), expected) in enumerate(test_cases):
+        assert Solution().resultsArray(nums, k) == expected
