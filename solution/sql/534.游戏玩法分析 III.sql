05ad882e8130ebfcec045f975ed3d5b0488945bb
颜涵 <yanhan0121@gmail.com>
2026-08-06 10:12:15 +0800

    add SQL solution for problem 534

---
+++ solution/sql/534.游戏玩法分析 III.sql
@@ -0,0 +1,7 @@
+SELECT a1.player_id, a1.event_date,
+       SUM(a2.games_played) AS games_played_so_far
+FROM Activity a1
+JOIN Activity a2
+  ON a2.player_id = a1.player_id
+ AND a2.event_date <= a1.event_date
+GROUP BY a1.player_id, a1.event_date;
