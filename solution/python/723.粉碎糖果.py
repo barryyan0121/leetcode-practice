b5eeb340444a4d58ccc65a5555a67acbc5f7c863
颜涵 <yanhan0121@gmail.com>
2026-08-07 13:31:14 +0800

    feat: add problem 723 solution

---
+++ solution/python/723.粉碎糖果.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=723 lang=python3
+#
+# [723] 粉碎糖果
+#
+
+
+# @lc code=start
+class Solution:
+    def candyCrush(self, board):
+        rows, cols = len(board), len(board[0])
+        while True:
+            crush = set()
+            for r in range(rows):
+                for c in range(cols - 2):
+                    value = abs(board[r][c])
+                    if value and value == abs(board[r][c + 1]) == abs(board[r][c + 2]):
+                        crush.update({(r, c), (r, c + 1), (r, c + 2)})
+            for c in range(cols):
+                for r in range(rows - 2):
+                    value = abs(board[r][c])
+                    if value and value == abs(board[r + 1][c]) == abs(board[r + 2][c]):
+                        crush.update({(r, c), (r + 1, c), (r + 2, c)})
+            if not crush:
+                return board
+            for r, c in crush:
+                board[r][c] = 0
+            for c in range(cols):
+                write = rows - 1
+                for r in range(rows - 1, -1, -1):
+                    if board[r][c]:
+                        board[write][c] = board[r][c]
+                        write -= 1
+                for r in range(write, -1, -1):
+                    board[r][c] = 0
+
+
+# @lc code=end
