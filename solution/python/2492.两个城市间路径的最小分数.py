73b8b4a5c840d0aaa61e6676fc11fe0c42210c72
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:53:18 +0800

    Add solution for problem 2492

---
+++ solution/python/2492.两个城市间路径的最小分数.py
@@ -0,0 +1,20 @@
+"""2492. 两个城市间路径的最小分数"""
+
+
+class Solution:
+    def minScore(self, n: int, roads: list[list[int]]) -> int:
+        graph = [[] for _ in range(n)]
+        for first, second, distance in roads:
+            graph[first - 1].append((second - 1, distance))
+            graph[second - 1].append((first - 1, distance))
+        seen = {0}
+        stack = [0]
+        answer = 10**9
+        while stack:
+            node = stack.pop()
+            for neighbor, distance in graph[node]:
+                answer = min(answer, distance)
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    stack.append(neighbor)
+        return answer
