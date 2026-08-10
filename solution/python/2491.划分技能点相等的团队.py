b3bb1036d1b99c400641005e404cfc1ff27ac43e
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:50:49 +0800

    Add solution for problem 2491

---
+++ solution/python/2491.划分技能点相等的团队.py
@@ -0,0 +1,10 @@
+"""2491. 划分技能点相等的团队"""
+
+
+class Solution:
+    def dividePlayers(self, skill: list[int]) -> int:
+        skill.sort()
+        target = skill[0] + skill[-1]
+        if any(skill[index] + skill[-index - 1] != target for index in range(len(skill) // 2)):
+            return -1
+        return sum(skill[index] * skill[-index - 1] for index in range(len(skill) // 2))
