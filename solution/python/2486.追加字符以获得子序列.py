622988336039ed3791d475c0b3003fb7dd99daec
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:41:14 +0800

    Add solution for problem 2486

---
+++ solution/python/2486.追加字符以获得子序列.py
@@ -0,0 +1,10 @@
+"""2486. 追加字符以获得子序列"""
+
+
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        matched = 0
+        for char in s:
+            if matched < len(t) and char == t[matched]:
+                matched += 1
+        return len(t) - matched
