c45bec3b770d58f3f5b487b795d8d2bb318b6008
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:31:58 +0800

    Format solution for problem 2482

--- solution/python/2482.行和列中一和零的差值.py
+++ solution/python/2482.行和列中一和零的差值.py
@@ -4,10 +4,16 @@
 class Solution:
     def onesMinusZeros(self, grid: list[list[int]]) -> list[list[int]]:
         rows = [sum(row) for row in grid]
-        cols = [sum(grid[row][col] for row in range(len(grid))) for col in range(len(grid[0]))]
+        cols = [
+            sum(grid[row][col] for row in range(len(grid)))
+            for col in range(len(grid[0]))
+        ]
         total_rows = len(grid[0])
         total_cols = len(grid)
         return [
-            [2 * (rows[row] + cols[col]) - total_rows - total_cols for col in range(len(cols))]
+            [
+                2 * (rows[row] + cols[col]) - total_rows - total_cols
+                for col in range(len(cols))
+            ]
             for row in range(len(rows))
         ]
