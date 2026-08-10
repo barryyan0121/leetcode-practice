5d1f6586520d683cff6519cb179a4df68f6fe105
颜涵 <yanhan0121@gmail.com>
2026-08-11 00:07:20 +0800

    Format solution for problem 2498

--- solution/python/2498.青蛙过河II.py
+++ solution/python/2498.青蛙过河II.py
@@ -5,4 +5,6 @@ class Solution:
     def maxJump(self, stones: list[int]) -> int:
         if len(stones) == 2:
             return stones[1] - stones[0]
-        return max(stones[index + 2] - stones[index] for index in range(len(stones) - 2))
+        return max(
+            stones[index + 2] - stones[index] for index in range(len(stones) - 2)
+        )
