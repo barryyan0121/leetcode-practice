4d9b736e8cbabe1a90ff66ca54f7f0f23e6c13bd
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:49:09 +0800

    Add solution for problem 2490

---
+++ solution/python/2490.回环句.py
@@ -0,0 +1,10 @@
+"""2490. 回环句"""
+
+
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split()
+        return all(
+            words[index][-1] == words[(index + 1) % len(words)][0]
+            for index in range(len(words))
+        )
