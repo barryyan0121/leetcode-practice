160f289e847fed04e5ba290aae135cde4eefaea5
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:58:26 +0800

    Add solution for problem 2495

---
+++ solution/python/2495.乘积为偶数的子数组数.py
@@ -0,0 +1,14 @@
+"""2495. 乘积为偶数的子数组数"""
+
+
+class Solution:
+    def evenProduct(self, nums: list[int]) -> int:
+        total = len(nums) * (len(nums) + 1) // 2
+        odd_run = odd_only = 0
+        for value in nums:
+            if value % 2:
+                odd_run += 1
+                odd_only += odd_run
+            else:
+                odd_run = 0
+        return total - odd_only
