19e9abcfa4f34bc12d1d590bebdf65b83384eb87
颜涵 <yanhan0121@gmail.com>
2026-08-10 10:21:24 +0800

    Fix solution for problem 1707

--- solution/python/1707.与数组中元素的最大异或值.py
+++ solution/python/1707.与数组中元素的最大异或值.py
@@ -1,25 +1,44 @@
-# @lc app=leetcode.cn id=1707 lang=python3
-
-
 class Solution:
-    def getMaximumXor(self, nums: list[int], maximumBit: int) -> list[int]:
-        mask = (1 << maximumBit) - 1
-        current = 0
-        for value in nums:
-            current ^= value
-        answer = []
-        for value in reversed(nums):
-            answer.append(mask ^ current)
-            current ^= value
+    def maximizeXor(self, nums: list[int], queries: list[list[int]]) -> list[int]:
+        nums.sort()
+        trie = [[-1, -1]]
+
+        def insert(value: int) -> None:
+            node = 0
+            for bit in range(30, -1, -1):
+                branch = (value >> bit) & 1
+                if trie[node][branch] == -1:
+                    trie[node][branch] = len(trie)
+                    trie.append([-1, -1])
+                node = trie[node][branch]
+
+        def best(value: int) -> int:
+            node = 0
+            result = 0
+            for bit in range(30, -1, -1):
+                branch = ((value >> bit) & 1) ^ 1
+                if trie[node][branch] == -1:
+                    branch ^= 1
+                result |= (branch ^ ((value >> bit) & 1)) << bit
+                node = trie[node][branch]
+            return result
+
+        ordered = sorted(enumerate(queries), key=lambda item: item[1][1])
+        answer = [-1] * len(queries)
+        index = 0
+        for query_index, (value, limit) in ordered:
+            while index < len(nums) and nums[index] <= limit:
+                insert(nums[index])
+                index += 1
+            if index:
+                answer[query_index] = best(value)
         return answer
 
 
 if __name__ == "__main__":
-    solution = Solution()
     test_cases = [
-        (solution.getMaximumXor, ([0, 1, 1, 3], 2), [0, 3, 2, 3]),
-        (solution.getMaximumXor, ([2, 3, 4, 7], 3), [5, 2, 6, 5]),
+        (([0, 1, 2, 3, 4], [[3, 1], [1, 3], [5, 6]]), [3, 3, 7]),
+        (([5, 2, 4, 6, 6, 3], [[12, 4], [8, 1], [6, 3]]), [15, -1, 5]),
     ]
-    for _, (func, args, expected) in enumerate(test_cases):
-        assert func(*args) == expected
-    print('第 1707 题 "与数组中元素的最大异或值" 所有测试用例通过')
+    for index, (args, expected) in enumerate(test_cases):
+        assert Solution().maximizeXor(*args) == expected, index
