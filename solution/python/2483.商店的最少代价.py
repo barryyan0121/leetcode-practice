c4a833f06706b233287df5fdcb6cb2fc12aa797a
颜涵 <yanhan0121@gmail.com>
2026-08-10 23:34:23 +0800

    Add solution for problem 2483

---
+++ solution/python/2483.商店的最少代价.py
@@ -0,0 +1,14 @@
+"""2483. 商店的最少代价"""
+
+
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        penalty = customers.count("Y")
+        best_penalty = penalty
+        answer = 0
+        for hour, customer in enumerate(customers):
+            penalty += 1 if customer == "N" else -1
+            if penalty < best_penalty:
+                best_penalty = penalty
+                answer = hour + 1
+        return answer
