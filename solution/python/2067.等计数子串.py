2578dd532e7b0fa531ffbf3530770b4438ff5738
Lint Action <lint-action@samuelmeuli.com>
2026-08-11 01:13:43 +0000

    Fix code style issues with Black

--- solution/python/2067.等计数子串.py
+++ solution/python/2067.等计数子串.py
@@ -10,6 +10,10 @@ class Solution:
                 freq[ord(char) - 97] += 1
                 if i >= length:
                     freq[ord(s[i - length]) - 97] -= 1
-                if i >= length - 1 and sum(x > 0 for x in freq) == unique and max(freq) == count:
+                if (
+                    i >= length - 1
+                    and sum(x > 0 for x in freq) == unique
+                    and max(freq) == count
+                ):
                     answer += 1
         return answer
