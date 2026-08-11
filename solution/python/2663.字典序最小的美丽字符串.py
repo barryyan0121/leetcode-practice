ec07e8abec64d575bf8940853a03357a16f2399c
颜涵 <yanhan0121@gmail.com>
2026-08-11 21:20:35 +0800

    feat: add solution for 2663

--- solution/python/2663.字典序最小的美丽字符串.py
+++ solution/python/2663.字典序最小的美丽字符串.py
@@ -1,26 +1,47 @@
-"""2663. 字典序最小的美丽字符串"""
+#
+# @lc app=leetcode.cn id=2663 lang=python3
+# @lcpr version=30203
+#
+# [2663] 字典序最小的美丽字符串
+#
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
 
 
+# @lc code=start
 class Solution:
     def smallestBeautifulString(self, s: str, k: int) -> str:
-        for index in range(len(s) - 1, -1, -1):
-            for code in range(ord(s[index]) + 1, ord("a") + k):
-                if index >= 1 and code == ord(s[index - 1]):
+        result = list(s)
+        for index in range(len(result) - 1, -1, -1):
+            for code in range(ord(result[index]) + 1, ord("a") + k):
+                if index >= 1 and code == ord(result[index - 1]):
                     continue
-                if index >= 2 and code == ord(s[index - 2]):
+                if index >= 2 and code == ord(result[index - 2]):
                     continue
-                answer = list(s[:index]) + [chr(code)]
-                for position in range(index + 1, len(s)):
+                result[index] = chr(code)
+                for next_index in range(index + 1, len(result)):
                     for next_code in range(ord("a"), ord("a") + k):
-                        if position >= 1 and next_code == ord(answer[position - 1]):
+                        if next_index >= 1 and next_code == ord(result[next_index - 1]):
                             continue
-                        if position >= 2 and next_code == ord(answer[position - 2]):
+                        if next_index >= 2 and next_code == ord(result[next_index - 2]):
                             continue
-                        answer.append(chr(next_code))
+                        result[next_index] = chr(next_code)
                         break
-                return "".join(answer)
+                return "".join(result)
         return ""
 
 
+# @lc code=end
+
+
 if __name__ == "__main__":
-    assert Solution().smallestBeautifulString("abcz", 26) == "abda"
+    solution = Solution()
+    assert solution.smallestBeautifulString("abcz", 26) == "abda"
+    assert solution.smallestBeautifulString("dc", 4) == ""
+    print("测试用例通过")
