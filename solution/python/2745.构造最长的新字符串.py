01e4c0696f762d8e268dcf7738cb4b651cfcc27d
颜涵 <yanhan0121@gmail.com>
2026-08-11 21:42:20 +0800

    feat: add solution for 2745

--- solution/python/2745.构造最长的新字符串.py
+++ solution/python/2745.构造最长的新字符串.py
@@ -1,10 +1,30 @@
-"""2745. 构造最长的新字符串"""
+#
+# @lc app=leetcode.cn id=2745 lang=python3
+# @lcpr version=30203
+#
+# [2745] 构造最长的新字符串
+#
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
 
 
+# @lc code=start
 class Solution:
     def longestString(self, x: int, y: int, z: int) -> int:
-        return 4 * min(x, y) + 2 * z + (2 if x != y else 0)
+        return 2 * (2 * min(x, y) + z + (x != y))
+
+
+# @lc code=end
 
 
 if __name__ == "__main__":
-    assert Solution().longestString(2, 5, 1) == 12
+    solution = Solution()
+    assert solution.longestString(2, 5, 1) == 12
+    assert solution.longestString(3, 2, 2) == 14
+    print("测试用例通过")
