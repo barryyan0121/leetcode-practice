04bda886333075fc46edbee96f279494f656a128
颜涵 <yanhan0121@gmail.com>
2026-08-11 22:00:58 +0800

    feat: add solution for 2832

--- solution/python/2832.每个元素为最大值的最大范围.py
+++ solution/python/2832.每个元素为最大值的最大范围.py
@@ -1,8 +1,22 @@
-"""2832. 每个元素为最大值的最大范围"""
+#
+# @lc app=leetcode.cn id=2832 lang=python3
+# @lcpr version=30203
+#
+# [2832] 每个元素为最大值的最大范围
+#
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
 
 
+# @lc code=start
 class Solution:
-    def maximumLengthOfRanges(self, nums: list[int]) -> list[int]:
+    def maximumLengthOfRanges(self, nums: List[int]) -> List[int]:
         n = len(nums)
         left = [-1] * n
         stack = []
@@ -11,6 +25,7 @@ class Solution:
                 stack.pop()
             left[index] = stack[-1] if stack else -1
             stack.append(index)
+
         right = [n] * n
         stack = []
         for index in range(n - 1, -1, -1):
@@ -21,5 +36,11 @@ class Solution:
         return [right[index] - left[index] - 1 for index in range(n)]
 
 
+# @lc code=end
+
+
 if __name__ == "__main__":
-    assert Solution().maximumLengthOfRanges([1, 5, 4, 3, 2]) == [1, 5, 3, 2, 1]
+    solution = Solution()
+    assert solution.maximumLengthOfRanges([1, 5, 4, 3, 6]) == [1, 4, 2, 1, 5]
+    assert solution.maximumLengthOfRanges([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    print("测试用例通过")
