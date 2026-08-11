5b3e98f2d9c26e07286cb5ab5eb082f8bacea5bc
颜涵 <yanhan0121@gmail.com>
2026-08-11 21:40:21 +0800

    feat: add solution for 2740

--- solution/python/2740.找出分区值.py
+++ solution/python/2740.找出分区值.py
@@ -1,11 +1,31 @@
-"""2740. 找出分区值"""
+#
+# @lc app=leetcode.cn id=2740 lang=python3
+# @lcpr version=30203
+#
+# [2740] 找出分区值
+#
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
 
 
+# @lc code=start
 class Solution:
-    def findValueOfPartition(self, nums: list[int]) -> int:
+    def findValueOfPartition(self, nums: List[int]) -> int:
         nums.sort()
         return min(right - left for left, right in zip(nums, nums[1:]))
 
 
+# @lc code=end
+
+
 if __name__ == "__main__":
-    assert Solution().findValueOfPartition([1, 3, 6, 10, 15]) == 2
+    solution = Solution()
+    assert solution.findValueOfPartition([1, 3, 2, 4]) == 1
+    assert solution.findValueOfPartition([100, 1, 10]) == 9
+    print("测试用例通过")
