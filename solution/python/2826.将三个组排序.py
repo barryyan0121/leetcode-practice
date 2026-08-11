aba96cf4efec1193b7e9fca5e1b3717c5a0e6eeb
颜涵 <yanhan0121@gmail.com>
2026-08-11 21:58:02 +0800

    feat: add solution for 2826

--- solution/python/2826.将三个组排序.py
+++ solution/python/2826.将三个组排序.py
@@ -1,15 +1,35 @@
-"""2826. 将三个组排序"""
+#
+# @lc app=leetcode.cn id=2826 lang=python3
+# @lcpr version=30203
+#
+# [2826] 将三个组排序
+#
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import *
+from common.node import *
 
 
+# @lc code=start
 class Solution:
-    def minimumOperations(self, nums: list[int]) -> int:
-        keep = [0, 0, 0]
+    def minimumOperations(self, nums: List[int]) -> int:
+        longest = [0, 0, 0]
         for value in nums:
-            keep[value - 1] += 1
-            keep[1] = max(keep[1], keep[0])
-            keep[2] = max(keep[2], keep[1])
-        return len(nums) - keep[2]
+            index = value - 1
+            longest[index] = max(longest[: index + 1]) + 1
+        return len(nums) - max(longest)
+
+
+# @lc code=end
 
 
 if __name__ == "__main__":
-    assert Solution().minimumOperations([2, 1, 3, 2, 1]) == 3
+    solution = Solution()
+    assert solution.minimumOperations([2, 1, 3, 2, 1]) == 3
+    assert solution.minimumOperations([1, 3, 2, 1, 3, 3]) == 2
+    assert solution.minimumOperations([2, 2, 2, 2, 3, 3]) == 0
+    print("测试用例通过")
