76728b2decf3f3ff7c1a7debb69ed0f6ea5d2b91
Lint Action <lint-action@samuelmeuli.com>
2026-08-13 03:16:14 +0000

    Fix code style issues with Black

--- solution/python/2889.数据重塑透视.py
+++ solution/python/2889.数据重塑透视.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    return weather.pivot(index="month", columns="city", values="temperature").reset_index()
+    return weather.pivot(
+        index="month", columns="city", values="temperature"
+    ).reset_index()
