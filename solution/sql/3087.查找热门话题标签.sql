75197e289951691de84603d63a4a994ef54c788b
颜涵 <yanhan0121@gmail.com>
2026-08-13 14:10:22 +0800

    feat: add solution for problem 3087

---
+++ solution/sql/3087.查找热门话题标签.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT('#', SUBSTRING_INDEX(SUBSTRING_INDEX(tweet, '#', -1), ' ', 1)) AS hashtag,
+       COUNT(*) AS hashtag_count
+FROM Tweets
+WHERE tweet_date >= '2024-02-01' AND tweet_date < '2024-03-01'
+GROUP BY hashtag
+ORDER BY hashtag_count DESC, hashtag DESC
+LIMIT 3;
