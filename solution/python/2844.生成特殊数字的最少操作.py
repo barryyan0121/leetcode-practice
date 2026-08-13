8aec5249e793cfbfa807665983868a4209b4c0f5
颜涵 <yanhan0121@gmail.com>
2026-08-13 10:23:30 +0800

    Add solution for LeetCode 2844

--- solution/python/2844.生成特殊数字的最少操作.py
+++ solution/python/2844.生成特殊数字的最少操作.py
@@ -4,20 +4,22 @@
 class Solution:
     def minimumOperations(self, num: str) -> int:
         answer = len(num)
-        for target in ("00", "25", "50", "75"):
+        for first, second in ("00", "25", "50", "75"):
             index = len(num) - 1
-            deletions = 0
-            for digit in reversed(target):
-                while index >= 0 and num[index] != digit:
-                    index -= 1
-                    deletions += 1
-                if index < 0:
-                    break
+            while index >= 0 and num[index] != second:
                 index -= 1
-            else:
-                answer = min(answer, deletions)
-        return answer
+            if index < 0:
+                continue
+            index -= 1
+            while index >= 0 and num[index] != first:
+                index -= 1
+            if index >= 0:
+                answer = min(answer, len(num) - index - 2)
+        return min(answer, len(num) - 1 if "0" in num else len(num))
 
 
 if __name__ == "__main__":
     assert Solution().minimumOperations("2245047") == 2
+    assert Solution().minimumOperations("2908305") == 3
+    assert Solution().minimumOperations("10") == 1
+    assert Solution().minimumOperations("1") == 1
