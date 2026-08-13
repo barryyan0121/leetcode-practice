3efe803b51a6adfe12d837deb3c059aa632a001f
颜涵 <yanhan0121@gmail.com>
2026-08-13 11:42:55 +0800

    feat: add solution for 2920

---
+++ solution/python/2920.收集所有金币可获得的最大积分.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumPoints(self, edges: list[list[int]], coins: list[int], k: int) -> int:
+        n = len(coins)
+        graph = [[] for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def dfs(node: int, parent: int, shift: int) -> int:
+            value = coins[node] >> shift
+            collect = value - k
+            halve = value >> 1
+            for child in graph[node]:
+                if child != parent:
+                    collect += dfs(child, node, shift)
+                    halve += dfs(child, node, shift + 1)
+            return max(collect, halve)
+
+        return dfs(0, -1, 0)
+
+
+assert Solution().maximumPoints([[0, 1], [1, 2]], [1, 2, 3], 1) == 2
