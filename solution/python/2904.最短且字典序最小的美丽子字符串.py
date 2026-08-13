853154ee42f9ac3ceb8486f5951152cfe4a61ec7
颜涵 <yanhan0121@gmail.com>
2026-08-13 11:26:37 +0800

    feat: add solution for 2904

---
+++ solution/python/2904.最短且字典序最小的美丽子字符串.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        left = ones = 0
+        answer = ""
+        for right, value in enumerate(s):
+            ones += value == "1"
+            while ones > k:
+                ones -= s[left] == "1"
+                left += 1
+            if ones == k:
+                while s[left] == "0":
+                    left += 1
+                candidate = s[left : right + 1]
+                if not answer or len(candidate) < len(answer) or (
+                    len(candidate) == len(answer) and candidate < answer
+                ):
+                    answer = candidate
+        return answer
+
+
+assert Solution().shortestBeautifulSubstring("100011001", 3) == "11001"
+assert Solution().shortestBeautifulSubstring("1011", 2) == "11"
