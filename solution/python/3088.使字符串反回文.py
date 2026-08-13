c01c3ab9af1cecfdd3203c3ed432996490d4b576
颜涵 <yanhan0121@gmail.com>
2026-08-13 14:13:31 +0800

    feat: add solution for problem 3088

---
+++ solution/python/3088.使字符串反回文.py
@@ -0,0 +1,21 @@
+class Solution:
+    def makeAntiPalindrome(self, s: str) -> str:
+        n = len(s)
+        half = n // 2
+        left = sorted(s)[:half]
+        counts = [0] * 26
+        for ch in s:
+            counts[ord(ch) - 97] += 1
+        for ch in left:
+            counts[ord(ch) - 97] -= 1
+        right = []
+        for i in range(half):
+            forbidden = ord(left[half - 1 - i]) - 97
+            for x in range(26):
+                if x != forbidden and counts[x]:
+                    right.append(chr(x + 97))
+                    counts[x] -= 1
+                    break
+            else:
+                return "-1"
+        return "".join(left + right)
