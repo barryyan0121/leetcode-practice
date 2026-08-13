97c54b9756e60f494dd65469eebb35a27f2fedb8
颜涵 <yanhan0121@gmail.com>
2026-08-13 17:25:39 +0800

    Add solution for problem 3840

---
+++ solution/python/3840.打家劫舍V.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int], colors: List[int]) -> int:
+        previous_two = 0
+        previous = 0
+        for i, value in enumerate(nums):
+            current = max(previous, value + (previous if i and colors[i] != colors[i - 1] else previous_two))
+            previous_two, previous = previous, current
+        return previous
+
+
+if __name__ == "__main__":
+    assert Solution().rob([1, 4, 3, 5], [1, 1, 2, 2]) == 9
+    assert Solution().rob([3, 1, 2, 4], [2, 3, 2, 2]) == 8
