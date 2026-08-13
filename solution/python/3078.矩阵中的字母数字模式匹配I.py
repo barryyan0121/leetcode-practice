b7f35c8bc34a45a965573cb1b8370fbd2d758ec3
颜涵 <yanhan0121@gmail.com>
2026-08-13 13:58:50 +0800

    feat: add solution for problem 3078

---
+++ solution/python/3078.矩阵中的字母数字模式匹配I.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findPattern(self, board: List[List[int]], pattern: List[str]) -> List[int]:
+        m, n = len(board), len(board[0])
+        h, w = len(pattern), len(pattern[0])
+        for r in range(m - h + 1):
+            for c in range(n - w + 1):
+                mapping, used = {}, set()
+                ok = True
+                for i in range(h):
+                    for j in range(w):
+                        ch, value = pattern[i][j], board[r + i][c + j]
+                        if ch.isdigit():
+                            ok = ok and int(ch) == value
+                        elif ch in mapping:
+                            ok = ok and mapping[ch] == value
+                        elif value in used:
+                            ok = False
+                        else:
+                            mapping[ch] = value
+                            used.add(value)
+                        if not ok:
+                            break
+                    if not ok:
+                        break
+                if ok:
+                    return [r, c]
+        return [-1, -1]
