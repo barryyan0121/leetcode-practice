6e29783c2d7f62c1632a7449f1e4668727ef4045
颜涵 <yanhan0121@gmail.com>
2026-08-13 11:50:24 +0800

    feat: add solution for 2925

---
+++ solution/python/2925.在树上执行操作以后得到的最大分数.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumScoreAfterOperations(
+        self, edges: list[list[int]], values: list[int]
+    ) -> int:
+        n = len(values)
+        graph = [[] for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def loss(node: int, parent: int) -> int:
+            children = [child for child in graph[node] if child != parent]
+            if not children:
+                return values[node]
+            return min(values[node], sum(loss(child, node) for child in children))
+
+        return sum(values) - loss(0, -1)
+
+
+assert (
+    Solution().maximumScoreAfterOperations(
+        [[0, 1], [0, 2], [0, 3], [2, 4], [4, 5]], [5, 2, 5, 2, 1, 1]
+    )
+    == 11
+)
