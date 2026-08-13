cdb038230379d2776a15d4a557acdadc3a07361b
颜涵 <yanhan0121@gmail.com>
2026-08-13 12:06:11 +0800

    feat: add solution for 2940

---
+++ solution/python/2940.找到Alice和Bob可以相遇的建筑.py
@@ -0,0 +1,21 @@
+class Solution:
+    def leftmostBuildingQueries(
+        self, heights: list[int], queries: list[list[int]]
+    ) -> list[int]:
+        answer = []
+        for a, b in queries:
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                answer.append(b)
+                continue
+            index = b + 1
+            while index < len(heights) and heights[index] <= heights[a]:
+                index += 1
+            answer.append(index if index < len(heights) else -1)
+        return answer
+
+
+assert Solution().leftmostBuildingQueries(
+    [6, 4, 8, 5, 2, 7], [[0, 1], [0, 3], [2, 4], [3, 4], [2, 2]]
+) == [2, 5, 5, 5, 2]
