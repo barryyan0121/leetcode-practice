9c988abed6fe0978793fe01363b932f0da01c2dd
颜涵 <yanhan0121@gmail.com>
2026-08-13 12:04:42 +0800

    feat: add solution for 2936

---
+++ solution/python/2936.包含相等值数字块的数量.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countBlocks(self, nums) -> int:
+        size = nums.size()
+        answer = 0
+        index = 0
+        while index < size:
+            value = nums.at(index)
+            left, right = index, size - 1
+            while left < right:
+                middle = (left + right + 1) // 2
+                if nums.at(middle) == value:
+                    left = middle
+                else:
+                    right = middle - 1
+            answer += 1
+            index = left + 1
+        return answer
