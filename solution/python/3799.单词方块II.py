6f9707a05961982f4b1c391c140f8f14005795e9
Lint Action <lint-action@samuelmeuli.com>
2026-08-13 07:28:08 +0000

    Fix code style issues with Black

--- solution/python/3799.单词方块II.py
+++ solution/python/3799.单词方块II.py
@@ -6,7 +6,12 @@ class Solution:
     def wordSquares(self, words: List[str]) -> List[List[str]]:
         answer = []
         for top, left, right, bottom in permutations(words, 4):
-            if top[0] == left[0] and top[3] == right[0] and bottom[0] == left[3] and bottom[3] == right[3]:
+            if (
+                top[0] == left[0]
+                and top[3] == right[0]
+                and bottom[0] == left[3]
+                and bottom[3] == right[3]
+            ):
                 answer.append([top, left, right, bottom])
         return sorted(answer)
 
