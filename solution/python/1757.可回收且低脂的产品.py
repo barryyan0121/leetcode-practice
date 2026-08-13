07bc81bba46134d13064df05a05020efd6891d76
颜涵 <yanhan0121@gmail.com>
2026-08-13 19:00:54 +0800

    Complete LeetCode 1628 and 1757

--- solution/python/1757.可回收且低脂的产品.py
+++ solution/python/1757.可回收且低脂的产品.py
@@ -2,6 +2,25 @@ import pandas as pd
 
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    return products[(products["low_fats"] == "Y") & (products["recyclable"] == "Y")][
-        ["product_id"]
+    return products.loc[
+        (products["low_fats"] == "Y") & (products["recyclable"] == "Y"),
+        ["product_id"],
     ]
+
+
+if __name__ == "__main__":
+    test_cases = [
+        (
+            pd.DataFrame(
+                {
+                    "product_id": [0, 1, 2, 3, 4],
+                    "low_fats": ["Y", "Y", "N", "Y", "N"],
+                    "recyclable": ["N", "Y", "Y", "Y", "N"],
+                }
+            ),
+            pd.DataFrame({"product_id": [1, 3]}),
+        ),
+    ]
+    for index, (products, expected) in enumerate(test_cases):
+        actual = find_products(products).reset_index(drop=True)
+        assert actual.equals(expected), index
